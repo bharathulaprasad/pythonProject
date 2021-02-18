16aaad39c0d95d5def59963537959936e69a1059
krishna.bharathula <krishnaprasad.bharathula@gmail.com>
2021-02-18 22:16:09 +0100

    prediction example using hadoop spark

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+from pyspark.sql import SparkSession
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
+def predictionexample():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    spark = SparkSession.builder.appName('Customers').getOrCreate()
+    dataset = spark.read.csv("Ecommerce_Customers.csv", inferSchema=True, header=True)
+    #build feature
+    featureassembler = VectorAssembler(inputCols=["Avg Session Length", "Time on App", "Time on Website", "Length of Membership"],outputCol="Independent Features")
+    output = featureassembler.transform(dataset)
+    output.show()
 
-
+    finalized_data = output.select("Independent Features", "Yearly Amount Spent")
+    finalized_data.show()
+    #split the data 80%, 20%
+    train_data, test_data = finalized_data.randomSplit([0.80, 0.20])
+    regressor = LinearRegression(featuresCol='Independent Features', labelCol='Yearly Amount Spent')
+    regressor = regressor.fit(train_data)
+    pred_results = regressor.evaluate(test_data)
+    pred_results.predictions.show(40)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    predictionexample()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
